2d06ea663d7e92ee4978c6d2ffe4d9cc3b337a06
Ceballosk <lvanessaoceballos@gmail.com>
2023-04-16 19:53:28 +0200

    mangler at fixe håndtering af telefon nr m.m.

--- test/test_main_web.py
+++ test/test_main_web.py
@@ -2,12 +2,16 @@
 import pytest
 
 import webscraping.main_web as main_web
+from webscraping.main_web import Spider
+
 
 
 def test_main():
     """Test main function"""
-    main_web.sik_ulr("https://www.sit.dk/")
+    # main_web.sik_ulr("https://www.sik.dk/")
+    test_class = Spider("https://www.sik.dk/")
     breakpoint()
+
 
 
 
--- webscraping/main_web.py
+++ webscraping/main_web.py
@@ -1,18 +1,37 @@
-
 from bs4 import BeautifulSoup
 import requests
-# import networkx as nx
 
-# class Spider: #TODO overvejer class
-# """Creating web crawler spider network from a website"""
-#     def __init__(self, website):
-#         self.website = website
-#         self.graph = nx.DiGraph()
-#         self.crawled = set()
-#         self.queue = set()  # queue of urls to crawl
-#         self.queue.add(website)
-#         self.crawl()
 
+class Spider:
+    """Creating web crawler spider network from a website"""
+
+    def __init__(self, website):
+        """Tilføje attributter til klassen"""
+        self.website = website
+        self.crawled = set()
+        self.queue = set()  # queue of urls to crawl
+        self.queue.add(website)  # tilføje website til queue
+        self.crawl()  # kører crawl metoden
+
+    def crawl(self):
+        """Crawl the website"""
+        # Iteration over the queue
+        while self.queue:
+            url = self.queue.pop()
+            self.crawled.add(url)
+            r = requests.get(url)
+            soup = BeautifulSoup(r.content, "html.parser")
+            links = soup.find_all("a")
+
+            # Iteration over links og derefter over attributterne i links, ved brug af .attrs (dictionary)
+            for link in links:
+                breakpoint()
+                if "href" in link.attrs:
+                    new_url = link.attrs["href"]
+                    if new_url not in self.crawled:
+                        self.queue.add(new_url)
+                        # TODO fix tel:+45 slet tel: fra new_url
+                        breakpoint()
 
 
 def sik_ulr(url):
@@ -20,10 +39,15 @@ def sik_ulr(url):
 
     # Define the website to crawl
     website = url
-    # Create a spider network
+    # Get the website content
     r = requests.get(website)
     # Create a BeautifulSoup object
     soup = BeautifulSoup(r.content, "html.parser")
     # Find all links in the website and save them in a list, looking for a-tags
     links = soup.find_all("a")
-    breakpoint()
+    # breakpoint()
+    # Print links ved at kigge efter href i attributterne
+    for link in links:
+        print(link["href"])
+        # if "href" in link.attrs:
+        #     print(link.attrs["href"])
